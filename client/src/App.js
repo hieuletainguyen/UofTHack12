9139bf81f140816f6f3d406749d18353ab660396
erin <aecmanalo@gmail.com>
2025-01-18 16:56:07 -0500

    hi

---
+++ client/src/App.js
@@ -0,0 +1,124 @@
+import React from "react";
+// import x5 from "./5.png";
+// import { StatusBarIphone } from "./StatusBarIphone";
+// import image1 from "./image-1.png";
+import "./style.css";
+
+export default function App() {
+    return (
+        <div className="home">
+            <div className="overlap-wrapper">
+                <div className="overlap">     
+                    {/* <StatusBarIphone
+                        background={false}
+                        batteryClassName="status-bar-iphone-4"
+                        cap="image.svg"
+                        capClassName="status-bar-iphone-7"
+                        capacityClassName="status-bar-iphone-6"
+                        cellularConnection="cellular-connection-2.svg"
+                        cellularConnectionClassName="status-bar-iphone-9"
+                        className="status-bar-iphone-instance"
+                        levelsClassName="status-bar-iphone-3"
+                        overlapGroupClassName="status-bar-iphone-5"
+                        timeClassName="design-component-instance-node"
+                        timeClassNameOverride="status-bar-iphone-2"
+                        wifi="wifi-2.svg"
+                        wifiClassName="status-bar-iphone-8"
+                    /> */}
+                    {/* <img className="image" alt="Image" src={image1} /> */}
+
+                    <div className="BG" />
+
+                    <div className="home-indicator" />
+
+                    <div className="rectangle" />
+
+                    <div className="div" />
+
+                    <div className="rectangle-2" />
+
+                    <div className="rectangle-3" />
+
+                    <div className="rectangle-4" />
+
+                    <div className="rectangle-5" />
+
+                    <div className="rectangle-6" />
+
+                    {/* <img className="element" alt="Element" src={x5} /> */}
+
+                    <div className="group">
+                        <div className="rectangle-7" />
+
+                        <div className="rectangle-8" />
+
+                        <div className="rectangle-9" />
+
+                        <div className="ellipse" />
+
+                        <div className="ellipse-2" />
+
+                        <div className="ellipse-3" />
+                    </div>
+
+                    <div className="group-2">
+                        <div className="rectangle-7" />
+
+                        <div className="rectangle-8" />
+
+                        <div className="rectangle-9" />
+
+                        <div className="ellipse" />
+
+                        <div className="ellipse-2" />
+
+                        <div className="ellipse-3" />
+                    </div>
+
+                    <div className="group-3">
+                        <div className="rectangle-7" />
+
+                        <div className="rectangle-8" />
+
+                        <div className="rectangle-9" />
+
+                        <div className="ellipse" />
+
+                        <div className="ellipse-2" />
+
+                        <div className="ellipse-3" />
+                    </div>
+
+                    <div className="frame">
+                        <div className="pfp">
+                            <div className="ellipse-4" />
+                        </div>
+
+                        <div className="text">
+                            <div className="text-wrapper-2">Hey, Jasmine!</div>
+
+                            <p className="p">welcome back, we’re happy to see you :)</p>
+                        </div>
+                    </div>
+
+                    {/* <StatusBarIphone
+                        background={false}
+                        batteryClassName="status-bar-iphone-4"
+                        cap="cap-2.svg"
+                        capClassName="status-bar-iphone-7"
+                        capacityClassName="status-bar-iphone-13"
+                        cellularConnection="cellular-connection-3.svg"
+                        cellularConnectionClassName="status-bar-iphone-9"
+                        className="status-bar-iphone-10"
+                        levelsClassName="status-bar-iphone-3"
+                        overlapGroupClassName="status-bar-iphone-12"
+                        timeClassName="design-component-instance-node"
+                        timeClassNameOverride="status-bar-iphone-11"
+                        wifi="wifi-3.svg"
+                        wifiClassName="status-bar-iphone-8"
+                    /> */}
+                </div>
+            </div>
+        </div>
+    );
+};
