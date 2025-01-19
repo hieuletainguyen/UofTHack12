612e8045caff3b48b565c36e9cea6cd91a4ab170
Kaustuv Pokharel <kastuvpokharel@gmail.com>
2025-01-18 22:58:16 -0500

    Testing 3D model loaded

--- testingtextspeech/src/components/model.js
+++ testingtextspeech/src/components/model.js
@@ -2,19 +2,31 @@ import React from 'react';
 import { Canvas } from '@react-three/fiber';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { useLoader } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
 
 const Model = () => {
+    // Point to the GLTF file in the public directory
     const modelUrl = `${process.env.PUBLIC_URL}/scene.gltf`;
+
+    // Load the model using useLoader
     const gltf = useLoader(GLTFLoader, modelUrl);
+
+    // Render the loaded GLTF model
     return <primitive object={gltf.scene} />;
 };
 
 const Scene = () => {
-  return (  
+  return (
     <Canvas>
-      <ambientLight />
-      <pointLight position={[10, 10, 10]} />
+      {/* Add some lights */}
+      <ambientLight intensity={0.5} />
+      <directionalLight position={[5, 5, 5]} />
+
+      {/* Render the model */}
       <Model />
+
+      {/* Enable interaction */}
+      <OrbitControls />
     </Canvas>
   );
 };
@@ -22,8 +34,8 @@ const Scene = () => {
 const App1 = () => {
   return (
     <div style={{ height: '100vh' }}>
+      {/* Render the 3D scene */}
       <Scene />
-      {/* <Model /> */}
     </div>
   );
 };
